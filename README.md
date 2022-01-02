9db2ff0443f3f42a5f21aeeab9d3d910dfa2e878
Aetheus <vidhunnagrajan@gmail.com>
2022-01-02 19:15:15 +0530

    PXBT_OCT HACKER

---
+++ README.md
@@ -0,0 +1,32 @@
+# Message spammer
+Instagram (desktop web version) / Discord / Whatsapp (desktop version) / Microsoft Teams message spammer.
+Please note that this script may work for other Messaging applications / websites as well, the ones listed above are the ones that tests have been conducted on.
+
+Windows:
+
+*Prerequisites*:
+None :)
+
+
+*Instructions*:
+Download the file named 'spammerX.X.exe', where 'X.X' is the version number, and run it, and then follow the on-screen instructions.
+
+*Linux/macOS/Non-Windows*:
+
+Prerequisites:
+Make sure latest Python AND Pynput version is installed.
+Install Pynput with 
+`pip3 install pynput`.    
+
+
+*Instructions*:
+Download the file named 'spammerX.X(non-windows).py', where 'X.X' is the version number, and run it, and then follow the on-screen instructions.
+
+
+Notes: 
+1. Time interval is measured in seconds.
+2. The non-windows script does in fact work on any Operating System, AS LONG AS the OS supports Python and the latest version of Python AND Pynput is installed.
+3. The non-windows script does work on Windows, but why use a Python script when you can use an .exe with no prior set-up?
+4. DO NOT MOVE THE MOUSE CURSOR WHILE IT IS SPAMMING.
+
+For suggestions/bugs, make an issue here: https://github.com/PXBT/MessageSpammer/issues
---
+++ spammer1.5(non-windows).py
@@ -0,0 +1,35 @@
+from time import sleep
+import sys
+import subprocess
+print("Made by Vidhun Nagarajan, 2021")
+sleep(0.2)
+print("Thanks to SpookySec for original code.")
+print("Version 1.5, Build Date: 17 Oct 2021")
+from pynput.mouse import Controller, Button
+from pynput import keyboard
+from pynput.keyboard import Key
+keyboard1 = keyboard.Controller()
+mouse = Controller()
+input1 = input("Enter what word(s) should be spammed and tap enter:")
+spamtime = float(input("Enter time interval (as decimal number, in seconds) between each sent message:"))
+spamvalue = int(input("Enter number of times to spam the word(s) you have entered:"))
+input("Place your mouse cursor on the typing area and tap enter:")
+bar = Controller().position
+print("Position of mouse cursor has been logged.")
+print("MAKE SURE YOU DON'T MOVE THE MOUSE!")
+input("Press enter to start spamming:")
+with open('words.txt', 'w') as f:
+    f.write(input1)
+file = open("words.txt", "r")
+lines = file.readlines()
+for i in range(spamvalue):
+    for line in lines:
+        word = str(line.rstrip())
+        mouse.position = bar
+        mouse.click(Button.left)
+        keyboard1.type(word)
+        keyboard1.press(Key.enter)
+        sleep(spamtime)
+print("Spamming completed!")
+wait = input("Press enter to close the script:")
+
